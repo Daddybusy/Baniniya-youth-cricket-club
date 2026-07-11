5157527bb91d9b6885d2ef2472622e5af2ebe2e2
Rahul Mandal <156097892+Daddybusy@users.noreply.github.com>
2026-07-11 14:45:30 +0545

    Update Firebase configuration with real credentials

--- firebase.js
+++ firebase.js
@@ -3,13 +3,15 @@ import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gs
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD9AarJiWz3Gr3GYpt9uC7nQZfjTRJDqh8",
+  authDomain: "baniniya-youth-cricket-c-fcf89.firebaseapp.com",
+  projectId: "baniniya-youth-cricket-c-fcf89",
+  storageBucket: "baniniya-youth-cricket-c-fcf89.firebasestorage.app",
+  messagingSenderId: "942736963772",
+  appId: "1:942736963772:web:1fc242256d980fd002b8b9",
+  measurementId: "G-ZWYGZJ98W5"
 };
 
 const app = initializeApp(firebaseConfig);
